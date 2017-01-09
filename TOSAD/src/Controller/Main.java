24d4b2547970c94178de6e0b928d4c609c2accea
jbsleepz <kotshond@gmail.com>
2017-01-09 15:35:08 +0100

    Operator Tests gescheiden van normale main.

--- TOSAD/src/Controller/Main.java
+++ TOSAD/src/Controller/Main.java
@@ -5,36 +5,7 @@ import Domain.Operator;
 public class Main {
 	public static void main(String[] args){
 		
-		//testen voor operator klasse
-		System.out.println("Test Operator klassen.");
-		Operator o = new Operator();
-		
-		int waardeA = 10;
-		int waardeB = 20;
-		int waardeTeChecken = 19;
-		String type = ">";
-		String StringWaardeA = "bla";
-		String StringWaardeB = "blabla";
-	
-		// in between test
-		System.out.println("De waarde ligt tussen" + waardeA + " en " + waardeB + " in? " + o.inBetween(waardeA, waardeB, waardeTeChecken));
-		
-		//biggerThen test
-		//type = ">=";
-		System.out.println("is de waarde " + waardeTeChecken + " groter dan " + waardeB + " = " + o.biggerThen(waardeTeChecken, waardeB, type));
-		
-		//lowerThen test
-		//type = "<";
-		type = "<=";
-		System.out.println("is de waarde " + waardeTeChecken + " kleiner dan " + waardeB + " = " + o.lowerThen(waardeTeChecken, waardeB, type));
-	
-		//StringwaardeA = "blabla";
-		//equals test
-		System.out.println("is de waarde gelijk? " + o.equalsTo(StringWaardeA, StringWaardeB));
-		
-		// StringWaardeA = "test"; StringWaardeB = "test";
-		//equals test
-		System.out.println("is de waarde gelijk? " + o.notEqualsTo(StringWaardeA, StringWaardeB));
+		System.out.println("Doet iets");
 				
 	}
 
