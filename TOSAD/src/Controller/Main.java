1e21c9ce6cdeaf6e8b0da3abc74478ec67628f0b
jellyforce <jelly.wie@gmail.com>
2017-01-23 17:31:09 +0100

    een werkende scriptgenerator voor rangerule

--- TOSAD/src/Controller/Main.java
+++ TOSAD/src/Controller/Main.java
@@ -1,11 +1,17 @@
 package Controller;
 
 import Domain.Operator;
+import Domain.RangeRule;
 
 public class Main {
 	public static void main(String[] args){
 		
 		System.out.println("Doet iets");
+		
+		//dit is een test van scriptoutput
+		RangeRule r = new RangeRule(123, 234);
+		String testScript = r.CreateRangeRuleScript("ATTRIBUTERANGERULE", "TEST", r.getMinValue(), r.getMaxValue());
+		System.out.println(testScript);
 				
 	}
 
--- TOSAD/src/Domain/RangeRule.java
+++ TOSAD/src/Domain/RangeRule.java
@@ -21,14 +21,47 @@ public class RangeRule extends Type{
 		return maximumValue;
 	}
 	
-	public String CreateRangeRuleScript(String tableName,String name, int min, int max){
-		String script = "CREATE OR REPLACE TRIGGER " +"TRIGGER_" + name + 
-						" BEFORE INSERT OR UPDATE ON " + tableName + 
-						" DECLARE"+ 
-						" MINVALUE NUMBER(10) = " + min + ";" + 
-						" MAXVALUE NUMBER(10) = " + max + ";" + 
-						" BEGIN"+ 
-						" IF :NEW.MINIMUMVALUE BETWEEN MINVALUE AND MAXVALUE";
+	public String CreateRangeRuleScript(String tableName,String triggerName, int min, int max){
+		String script = "CREATE OR REPLACE TRIGGER " +"TRIGGER_" + triggerName + 
+						" \nBEFORE INSERT OR UPDATE ON " + tableName + 
+						" \nFOR EACH ROW" +
+						" \nDECLARE"+ 
+						" \nMINVALUE NUMBER := " + min + ";" + 
+						" \nMAXVALUE NUMBER := " + max + ";" + 
+						" \nMINIMUMVALUE NUMBER;" +
+						" \nMAXIMUMVALUE NUMBER;" +
+						" \nBEGIN"+ 
+						" \nMINIMUMVALUE := :NEW.MINIMUMVALUE;" +
+						" \nMAXIMUMVALUE := :NEW.MAXIMUMVALUE;" +
+						" \nIF (MINIMUMVALUE < MINVALUE OR MINIMUMVALUE > MAXVALUE)" + 
+						" \nTHEN " + 
+						" \nRaise_Application_Error(-20000, 'minimum value is not a valid number');" + //exceptionmessage
+						" \nELSIF (MAXIMUMVALUE < MINVALUE OR MAXIMUMVALUE > MAXVALUE)" +
+						" \nTHEN " + 
+						" \nRaise_Application_Error(-20000, 'maximum value is not a valid number');" + //exceptionmessage
+						" \nEND IF;" +
+						" \nEND;";
+		/*
+		create or replace TRIGGER TRIGGER_TEST 
+		BEFORE INSERT OR UPDATE ON ATTRIBUTERANGERULE
+		FOR EACH ROW
+		DECLARE 
+		MINVALUE NUMBER := 123; 
+		MAXVALUE NUMBER := 234; 
+		MINIMUMVALUE NUMBER;
+		MAXIMUMVALUE NUMBER;
+		BEGIN 
+		MINIMUMVALUE := :NEW.MINIMUMVALUE;
+		MAXIMUMVALUE := :NEW.MAXIMUMVALUE;
+		IF (MINIMUMVALUE < MINVALUE OR MINIMUMVALUE > MAXVALUE)
+		THEN 
+		RAISE_APPLICATION_ERROR(-20000, 'minimum value is not a valid number');
+		ELSIF (MAXIMUMVALUE < MINVALUE OR MAXIMUMVALUE > MAXVALUE)
+		THEN
+		RAISE_APPLICATION_ERROR(-20001, 'maximum value is not a valid number');
+		END IF; 
+		END;
+*/
 					
 		
 	
