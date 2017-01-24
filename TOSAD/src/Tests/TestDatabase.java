43a8f5ac13e440df5faf0fd33c68c32e75f395de
jbsleepz <kotshond@gmail.com>
2017-01-24 17:10:55 +0100

    Jar committen

--- TOSAD/src/Tests/TestDatabase.java
+++ TOSAD/src/Tests/TestDatabase.java
@@ -16,14 +16,9 @@ public class TestDatabase extends ConnectionConfiguration {
 	private static final String DB_HOST = "ondora02.hu.nl";
 	private static final String DB_PORT = "8080";
 	private static final String DB_PASS = "pizza";
-	private static Connection conn;
-	private static final String connectieString = "jdbc:oralce:thin:tosad_2016_2c_team3/tosad_2016_2c_team3@//ondora02.hu.nl:8521/cursus02.hu.nl";
-	"jdbc:oracle:thin:tosad_2016_2C_team3/pizza@//ondora02.hu.nl:8521/cursus02.hu.nl";		  
-	
-/*	String dbSchemeName = "tosad_2016_2C_team3", 
-			connectionURL = "ondora02.hu.nl" + dbSchemeName,
-			username = "tosad_2016_2C_team3", password = "pizza";*/
-	
+	private static final String connectieString = "jdbc:oracle:thin:tosad_2016_2c_team3_target/tosad_2016_2c_team3_target@//ondora02.hu.nl:8521/cursus02.hu.nl";
+/*												  "jdbc:oracle:thin:tosad_2016_2C_team3/pizza@//ondora02.hu.nl:8521/cursus02.hu.nl";	*/	  
+
 	// De methode die met JDBC aan de slag gaat moet een SQLException opvangen of gooien
 	public static void main(String[] args) throws SQLException{
 		//Besluit welke driver je gaat gebruiken voor je verbinding		
@@ -33,11 +28,12 @@ public class TestDatabase extends ConnectionConfiguration {
 		catch (InstantiationException | IllegalAccessException | ClassNotFoundException e1) {
 			e1.printStackTrace();
 		}
-		
+		System.out.println("kom je hier?");
 		// Leg de connectie met de database
 		/*conn = DriverManager.getConnection("DB_URL ,DB_USER, DB_PASS");*/
-		conn = DriverManager.getConnection(connectieString);
-	
+		Connection conn = DriverManager.getConnection(connectieString);
+
+		System.out.println("kom je hier? 2");
 		//con = DriverManager.getConnection("jdbc:oracle:thin:@//" + host + ":" + port +"/"+database+"?user="+user+"&password="+password);
         
 		//System.out.println("Connection made");
@@ -45,7 +41,7 @@ public class TestDatabase extends ConnectionConfiguration {
 		// Een eerste SQL statement maken
 		Statement stmt = conn.createStatement();		
 		// Een tweede statement maken dat een resultaat oplevert
-		String queryText = "SELECT TESTDATA FROM TESTDATA";
+		String queryText = "SELECT 1 FROM dual";
 		
 		// Een tweede statement uitvoeren
 		ResultSet rs = stmt.executeQuery(queryText);
