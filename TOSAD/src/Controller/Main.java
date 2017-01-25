15145c799fb63696f9798b8c94435739911bc5eb
jellyforce <jelly.wie@gmail.com>
2017-01-25 13:09:43 +0100

    werkende listRule scriptgenerator voor strings

--- TOSAD/src/Controller/Main.java
+++ TOSAD/src/Controller/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import Domain.Attribute;
 import Domain.CompareRule;
+import Domain.ListRule;
 import Domain.Operator;
 import Domain.RangeRule;
 
@@ -12,16 +13,22 @@ public class Main {
 		
 
 		
-		ArrayList<String> al = new ArrayList<String>();
+		/*ArrayList<String> al = new ArrayList<String>();
 		al.add("een");
 		al.add("twee");
 		
 		CompareRule c = new CompareRule();
 		String testScript1 = c.createAttributeCompareScript("test_name", "test_table", "test_value", ">");
 		System.out.println(testScript1);
+		*/
 		
 		
-				
+		//testing listrule parser
+		String test  = "bla asd nogeen string";
+		ListRule l = new ListRule(test);
+		ArrayList<String> listValues = l.stringToListParser(test);	
+		String script = l.createAttributeListRuleScript("ListRule", "Some_table", "Some_Column", listValues);
+		System.out.println(script);
 	}
 
 }
--- TOSAD/src/Domain/ListRule.java
+++ TOSAD/src/Domain/ListRule.java
@@ -1,5 +1,72 @@
 package Domain;
 
+import java.util.ArrayList;
+
 public class ListRule extends RuleType{
 	Operator operator = new Operator();
+	private String stringValues;
+	private ArrayList<String> listValues = new ArrayList<String>();
+
+	
+	public ListRule(String stringValues){
+		this.stringValues = stringValues;
+	}
+	
+	
+	public ArrayList<String> stringToListParser(String stringValues){
+		String delimmiter = "[ ]+";
+		String[] tokens  = stringValues.split(delimmiter);
+		
+		for(String s : tokens){
+			listValues.add(s);  //place string items as separate value in a list
+		}		
+		return listValues;
+	}
+	
+	
+	//nodig:
+	//lijst met values (string)
+	//tabelnaam
+	//kolomnaam
+	
+	public String createAttributeListRuleScript(String triggerName, String tableName, String columnName, ArrayList<String> listValues){
+		String list = "";
+		int length = listValues.size();
+		int counter = 1;
+		for(String s : listValues){
+			if(counter == length){
+				list += s; // there is not an extra "," needed if it is the last item from the list
+				break;
+			}
+			else{
+				list += s + ", ";
+				counter +=1;
+			}
+			
+		}
+		
+		String script = "CREATE OR REPLACE TRIGGER trigger_" + triggerName + 
+						"\nBEFORE INSERT OR UPDATE ON " + tableName + 
+						"\nFOR EACH ROW" + 
+						"\nDECLARE" + 
+						"\nCOLUMNVALUE VARCHAR2" +
+						"\nBEGIN" + 
+						"\nCOLUMNVALUE := NEW." + columnName +  //the new value will be stored in here
+						"\nIF INSTR('"+list + "', COLUMNVALUE) < 1" + //if given value IS NOT part of the list
+						"\nTHEN" +
+						"\nRAISE_APPLICATION_ERROR(20003,'the given value must be one of the following: '"+list+"  )" +
+						"\nEND IF" +
+						"\nEND";
+		
+		
+		
+
+						// waarde van de kolom moet gelijk zijn aan 1 van die van de lijst
+						
+		
+		
+				
+				
+		return script;
+	}
 }
