036da47af80df6aa948c5cfb05f369bb34b4b471
jbsleepz <kotshond@gmail.com>
2017-01-27 14:55:08 +0100

    listener

--- TOSAD/src/Tests/TestHashMap.java
+++ TOSAD/src/Tests/TestHashMap.java
@@ -17,23 +17,29 @@ public class TestHashMap {
 		m1.put("mathew", "Hyden");
 		m1.put("schildt", "java2s");
 		
+		Map m2 = new HashMap();
+		m2.put("blabla", "HashMap");
+		m2.put("blabla2", "java2");
+		m2.put("blabla3", "Hyden");
+		m2.put("blabla4", "java2s");
+		
 		System.out.println(m1.keySet()); 
-		System.out.println(m1.values()); 
+		System.out.println(m1.values() + "\n"); 
 		
-		
+	/*	
 		SortedMap sm = new TreeMap();
 		sm.put("map", "TreeMap");
 		sm.put("schildt", "java2");
 		sm.put("mathew", "Hyden");
 		sm.put("schildt", "java2s");
 		System.out.println(sm.keySet()); 
-		System.out.println(sm.values());
+		System.out.println(sm.values());*/
 
-		LinkedHashMap lm = new LinkedHashMap();
-		lm.put("map", "LinkedHashMap");
+		LinkedHashMap lm = new LinkedHashMap(m1);
+/*		lm.put("map", "LinkedHashMap");
 		lm.put("schildt", "java2");
 		lm.put("mathew", "Hyden");
-		lm.put("schildt", "java2s");
+		lm.put("schildt", "java2s");*/
 		System.out.println(lm.keySet()); 
 		System.out.println(lm.values());
 	}
