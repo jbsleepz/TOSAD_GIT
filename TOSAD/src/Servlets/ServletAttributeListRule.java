28b2c116ae944195ba4d9900b6095c589fdd2eab
Benzuin <Benzuin@192.168.1.15>
2017-01-30 21:13:09 +0100

    servlets

--- TOSAD/src/Servlets/ServletAttributeListRule.java
+++ TOSAD/src/Servlets/ServletAttributeListRule.java
@@ -2,18 +2,29 @@ package Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import Domain.FacadeDomain;
 
 public class ServletAttributeListRule extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			 throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
-		out.print("in progress");
+		String ruletype = "List";
+		String rulename = req.getParameter("RuleName");
+		String ColumnWaarde = req.getParameter("ColumnWaarde");
+		String tablewaarde = req.getParameter("TableWaarde");
+		String listvalues = req.getParameter("ListValues");
+		ArrayList<String> list = new ArrayList<String>();
+		list.add(listvalues);
+		String errormessage = req.getParameter("ErrorMessage");
+		FacadeDomain facade = new FacadeDomain();
+		out.print(facade.MaaktScriptVoorAttributeList(ruletype, rulename, ColumnWaarde, tablewaarde, list, errormessage));
 	}
 
 }
--- TOSAD/src/Servlets/ServletModifyRule.java
+++ TOSAD/src/Servlets/ServletModifyRule.java
@@ -14,7 +14,17 @@ public class ServletModifyRule extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			 throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
-		out.print("in progress");
+		String ruletype = "Modify";
+		String rulename = req.getParameter("BusinessruleName");
+		String errormessage = req.getParameter("ErrorMessage");
+		String tablename1 = req.getParameter("TargetTableName_1");
+		String tablename2 = req.getParameter("TargetTableName_2");
+		String targetcolumn_1 = req.getParameter("TargetColumn_1");
+		String targetcolumn_2 = req.getParameter("TargetColumn_2");
+		String operator = req.getParameter("Operator");
+		String checkwaarde = req.getParameter("CheckWaarde");
+		FacadeDomain facade = new FacadeDomain();
+		out.print(facade.maakScriptVoorModifyRule(ruletype, operator, rulename, tablename1, tablename2, targetcolumn_1, targetcolumn_2, checkwaarde, errormessage));
 	}
 
 }
